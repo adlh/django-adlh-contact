4083e28e9cb205ceeb6db45246de1d63a5f96e5d
Andrea de la Huerta <info@metamorfosys.de>
2025-02-09 19:58:45 +0100

    Update some s/ugettext_lazy/gettext_lazy/ imports
    And remove build dir from the repository.

--- build/lib/contact/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from django.views.generic import TemplateView
-from .views import ContactFormView
-from . import views
-
-urlpatterns = [
-    path(r'', ContactFormView.as_view(), name='contact'),
-    path(r'thanks/', TemplateView.as_view(template_name='contact/thanks.html'), name='thanks'),
-]
--- src/contact/urls.py
+++ src/contact/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path, include
+from django.urls import path
 from django.views.generic import TemplateView
 from .views import ContactFormView
-from . import views
 
 urlpatterns = [
     path(r'', ContactFormView.as_view(), name='contact'),
